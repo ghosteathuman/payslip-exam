7e719e52b000deecc72e861ef499e12e8da8f223
ghosteathuman <46582963+ghosteathuman@users.noreply.github.com>
2020-08-25 23:03:53 +0800

    Extract income tax calculation to another method and add tests.

--- requirement-1/payslip.rb
+++ requirement-1/payslip.rb
@@ -5,25 +5,27 @@ class Payslip
       puts "Invalid input. Please ensure that the name is a String and annual salary is not negative"
     end
 
-    if annual_salary <= 20000
-      annual_income_tax = 0
-    elsif annual_salary >= 180001
-      annual_income_tax = 40000 + ((annual_salary - 180000) * 0.4)
-    elsif annual_salary <= 40000
-      annual_income_tax = ((annual_salary - 20000) * 0.1)
-    elsif annual_salary <= 80000
-      annual_income_tax =  2000 + ((annual_salary - 40000) * 0.2)
-    elsif annual_salary <= 180000
-      annual_income_tax = 10000 + ((annual_salary - 80000) * 0.3)
-    end
-
     gross_monthly_income = annual_salary / 12
-    monthly_income_tax = annual_income_tax / 12
+    monthly_income_tax = calculate_income_tax(annual_salary) / 12
     net_monthly_income = gross_monthly_income - monthly_income_tax
 
     puts "Monthly Payslip for: \"#{employee_name}\""
     puts "Gross Monthly Income: $#{sprintf('%.2f', gross_monthly_income)}"
     puts "Monthly Income Tax: $#{sprintf('%.2f', monthly_income_tax)}"
     puts "Net Monthly Income: $#{sprintf('%.2f', net_monthly_income)}"
+  end
+
+  def calculate_income_tax (annual_salary)
+    if annual_salary <= 20000
+      return 0
+    elsif annual_salary >= 180001
+      return 40000 + ((annual_salary - 180000) * 0.4)
+    elsif annual_salary <= 40000
+      return ((annual_salary - 20000) * 0.1)
+    elsif annual_salary <= 80000
+      return 2000 + ((annual_salary - 40000) * 0.2)
+    elsif annual_salary <= 180000
+      return 10000 + ((annual_salary - 80000) * 0.3)
+    end
   end
 end
